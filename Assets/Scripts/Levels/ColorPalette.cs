24204e929eac3cf7268c75f6b2871028a28b650b
barisdincer <barisdincer@sabanciuniv.edu>
2019-10-27 14:18:07 +0100

    Assign colors to color palettes dynamically

---
+++ Assets/Scripts/Levels/ColorPalette.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPalette : MonoBehaviour {
+
+    private void Awake()
+    {
+
+        string currentObjectTag = this.tag;
+        switch (currentObjectTag)
+        {
+            case "YellowChanger":
+                gameObject.GetComponent<Renderer>().material.color = Constants.Yellow;
+                break;
+            case "GreenChanger":
+                gameObject.GetComponent<Renderer>().material.color = Constants.Green;
+                break;
+            case "BlueChanger":
+                gameObject.GetComponent<Renderer>().material.color = Constants.Blue;
+                break;
+            case "RedChanger":
+                gameObject.GetComponent<Renderer>().material.color = Constants.Red;
+                break;
+        }
+    }
+}
